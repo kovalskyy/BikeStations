49b4f661db9d8adcf1d28e8dac595279c0715c7d
Kacper Kowalski <kowal@Mac-mini-Kacper.local>
2018-08-01 13:05:49 +0200

    Prepare tableView

--- BikeStationTest/ViewController/BikeStationListViewController.swift
+++ BikeStationTest/ViewController/BikeStationListViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class BikeStationListViewController: UIViewController  {
+class BikeStationListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.register(UINib(nibName: "BikeStationTableViewCell", bundle: nil), forCellReuseIdentifier: "BikeStationTableViewCell")
 
     }
+    
+    // MARK: - UITableView data source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "BikeStationTableViewCell") as! BikeStationTableViewCell
+        
+        cell.item = nil
+        
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
 
 }
 
